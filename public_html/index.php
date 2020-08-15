8582f89abbb3fb70fa075ed3fbe8395572b5e286
Wani Joseph <waynekhlifa.wk@gmail.com>
2020-08-15 08:39:20 +0200

    initial commit

---
+++ public_html/index.php
@@ -0,0 +1,13 @@
+<h1>Hello Osman!</h1>
+<h4>Attempting MySQL connection from php...</h4>
+<!--?php 
+$host = 'mysql';
+$user = 'root';
+$pass = 'rootpassword';
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected to MySQL successfully!";
+?-->
